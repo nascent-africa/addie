532e9f01a933509af2990baa210632326d1fdfc2
tsommie <anitchec.dev@gmail.com>
2020-08-24 15:58:36 +0000

    Fix error.

--- resources/views/pages/localGovernmentArea/show.blade.php
+++ resources/views/pages/localGovernmentArea/show.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
     @component('pages.localGovernmentArea.wrapper')
-        @if (isset($city))
+        @if (isset($localGovernmentArea))
             @slot('actions')
-                <a class="btn btn-sm btn-outline-secondary ml-3" href="{{ route('local_government_areas.edit', $city) }}">
+                <a class="btn btn-sm btn-outline-secondary ml-3" href="{{ route('local_government_areas.edit', $localGovernmentArea) }}">
                     <span data-feather="edit"></span>
                 </a>
 
                 <x-delete-button
                     classes="btn btn-sm btn-outline-danger ml-3"
-                    name="{{ $city->name }}"
-                                 url="{{ route('local_government_areas.destroy', $city) }}"
-                                 id="{{ $city->slug }}"></x-delete-button>
+                    name="{{ $localGovernmentArea->name }}"
+                                 url="{{ route('local_government_areas.destroy', $localGovernmentArea) }}"
+                                 id="{{ $localGovernmentArea->slug }}"></x-delete-button>
             @endslot
         @endif
 
         <div class="row">
             <div class="col-12">
-                <h3>{{ $city->name }}</h3>
+                <h3>{{ $localGovernmentArea->name }}</h3>
 
-                <span class="font-weight-bold">Country:</span> <span class="text-muted">{{ $city->country->name }}</span> &nbsp; | &nbsp;
-                <span class="font-weight-bold">Latitude:</span> <span class="text-muted">{{ $city->latitude }}</span> &nbsp; | &nbsp;
-                <span class="font-weight-bold">Longitude:</span> <span class="text-muted">{{ $city->longitude }}</span> &nbsp; | &nbsp;
+                <span class="font-weight-bold">Country:</span> <span class="text-muted">{{ $localGovernmentArea->country->name }}</span> &nbsp; | &nbsp;
+                <span class="font-weight-bold">Latitude:</span> <span class="text-muted">{{ $localGovernmentArea->latitude }}</span> &nbsp; | &nbsp;
+                <span class="font-weight-bold">Longitude:</span> <span class="text-muted">{{ $localGovernmentArea->longitude }}</span> &nbsp; | &nbsp;
             </div>
         </div>
         <!-- /.row -->
